14764b1419248637e3eb361c785521c811abcf5f
Jake Blozan <jakeblozan>
2016-10-27 19:22:04 -0700

    rabbits update

--- RabbitsandRecurrenceRelations.java
+++ RabbitsandRecurrenceRelations.java
@@ -12,6 +12,23 @@ public class RabbitsandRecurrenceRelations
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(new File("practice.txt"));
         System.out.println("Fetching data...");
-        
+        int mRabbits = 0;
+        int yRabbits = 1;
+        //        int y2Rabbits = 0;
+        int months = scan.nextInt();
+        int birthrate = scan.nextInt();
+        for (int i = 0; i < months; i++) {
+            if (i == 0) {
+
+            }
+            else {
+                mRabbits += yRabbits;
+                yRabbits = mRabbits * birthrate;
+            }
+            System.out.println(mRabbits);
+            System.out.println(yRabbits);
+            System.out.println("");
+        }
+        System.out.println(mRabbits + yRabbits);
     }
 }
