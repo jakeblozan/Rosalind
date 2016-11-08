9a86e2c123e538a62f62a2e3646fdd7ad52c902d
Jake Blozan <jakeblozan@users.noreply.github.com>
2016-11-08 10:00:14 -0800

    GC Content update

--- ComputingGCContent.java
+++ ComputingGCContent.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * Write a description of class ComputingGCContent here.
@@ -17,11 +18,30 @@ public class ComputingGCContent
         for (int i = 0; i < length; i++) {
             if (data.charAt(i) == '>') {
                 strings++;
+
+            }
+        }
+        int[] indexes = new int[strings];
+        int counter = 0;
+        for (int i = 0; i < length; i++) {
+            if (data.charAt(i) == '>') {
+                indexes[counter] = i;
+                counter++;
             }
         }
         String[] tags = new String[strings];
         for (int i = 0; i < strings; i++) {
-            
+            tags[i] = data.substring((indexes[i] + 1), (indexes[i] + 14));
         }
+        String[] content = new String[strings];
+        for (int i = 0; i < strings; i++) {
+            if (i < (strings - 1)) {
+                content[i] = data.substring((indexes[i] + 15), (indexes[i + 1] - 1));
+            }
+            else {
+                content[i] = data.substring((indexes[i] + 15), data.length());
+            }
+        }
+        System.out.println(Arrays.toString(content));
     }
 }
