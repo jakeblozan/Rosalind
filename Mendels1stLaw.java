80dc2afc0652dbfbba45653d9a05635354c9ca68
Jake Blozan <jakeblozan>
2016-12-06 22:51:50 -0800

    mendel complete

--- Mendels1stLaw.java
+++ Mendels1stLaw.java
@@ -18,25 +18,16 @@ public class Mendels1stLaw
         double rec = scan.nextDouble();
         double total = dom + het + rec;
         System.out.println(total);
-        double probDomDom = (dom / total) * (dom / (total - 1));
-        System.out.println(probDomDom);
+        double probDomDom = (dom / total) * ((dom - 1) / (total - 1));
         double probDomHet = (dom / total) * (het / (total - 1));
-        System.out.println(probDomHet);
         double probDomRec = (dom / total) * (rec / (total - 1));
-        System.out.println(probDomRec);
         double probHetDom = (het / total) * (dom / (total - 1));
-        System.out.println(probHetDom);
-        double probHetHet = (het / total) * (het / (total - 1));
-        System.out.println(probHetHet);
+        double probHetHet = (het / total) * ((het - 1) / (total - 1));
         double probHetRec = (het / total) * (rec / (total - 1));
-        System.out.println(probHetRec);
         double probRecDom = (rec / total) * (dom / (total - 1));
-        System.out.println(probRecDom);
         double probRecHet = (rec / total) * (het / (total - 1));
-        System.out.println(probRecHet);
-        double probRecRec = (rec / total) * (rec / (total - 1));
-        System.out.println(probRecRec);
-        double totalProb = probDomDom + probDomHet + probDomRec + probHetDom + probHetHet + probHetRec + probRecDom + probRecHet + probRecRec;
-        System.out.println(totalProb);
+        double probRecRec = (rec / total) * ((rec - 1) / (total - 1));
+        double odds = probDomDom + probDomHet + probDomRec + probHetDom + (probHetHet * 0.75) + (probHetRec * 0.5) + probRecDom + (probRecHet * 0.5);
+        System.out.println(odds);
     }
 }
