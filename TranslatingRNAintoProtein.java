0edc5610e4c0e0ef5e15c587fff45ea9a6df7685
Jake Blozan <jakeblozan@gmail.com>
2016-12-07 12:03:27 -0800

    rna to protein initial

---
+++ TranslatingRNAintoProtein.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Write a description of class TranslatingRNAintoProtein here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TranslatingRNAintoProtein
+{
+    public static void main(String[] args) throws IOException {
+        Scanner data = new Scanner(new File("practice.txt"));
+        Scanner tableScan = new Scanner(new File("table.txt"));
+        System.out.println("Fetching data...");
+        
+    }
+}
