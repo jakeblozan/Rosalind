3ce7a2f93a1f0033577f9f92264fb2575178df14
Jake Blozan <jakeblozan@gmail.com>
2016-12-08 10:29:06 -0800

    transRNAupdate + table

--- TranslatingRNAintoProtein.java
+++ TranslatingRNAintoProtein.java
@@ -13,6 +13,14 @@ public class TranslatingRNAintoProtein
         Scanner data = new Scanner(new File("practice.txt"));
         Scanner tableScan = new Scanner(new File("table.txt"));
         System.out.println("Fetching data...");
-        
+        String[] table = new String[64];
+        for (int i = 0; i < 64; i++) {
+            table[i] = tableScan.nextLine();
+        }
+        String allData = data.next();
+        int codons = allData.length() / 3;
+        System.out.println(allData);
+        System.out.println(allData.length());
+        System.out.println(codons);
     }
 }
