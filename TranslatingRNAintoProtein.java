a7dcad3b65b9329c0a5f7b25e95b206dfa5125cb
Jake Blozan <jakeblozan@gmail.com>
2016-12-09 10:03:50 -0800

    transRNAprot update

--- TranslatingRNAintoProtein.java
+++ TranslatingRNAintoProtein.java
@@ -4,8 +4,8 @@ import java.io.*;
 /**
  * Write a description of class TranslatingRNAintoProtein here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Jake Blozan) 
+ * @version (12/9/16)
  */
 public class TranslatingRNAintoProtein
 {
@@ -13,14 +13,48 @@ public class TranslatingRNAintoProtein
         Scanner data = new Scanner(new File("practice.txt"));
         Scanner tableScan = new Scanner(new File("table.txt"));
         System.out.println("Fetching data...");
-        String[] table = new String[64];
-        for (int i = 0; i < 64; i++) {
-            table[i] = tableScan.nextLine();
+        String[] tableInit = new String[128];
+        for (int i = 0; i < 128; i++) {
+            tableInit[i] = tableScan.next();
+        }
+        boolean yesNoRNA = true;
+        String[] RNA = new String[64];
+        int which = 0;
+        for (int i = 0; i < 128; i++){
+            if (yesNoRNA) {
+                RNA[which] = tableInit[i];
+                which++;
+            }
+            yesNoRNA = !yesNoRNA;
+        }
+        boolean yesNoProtein = false;
+        String[] proteins = new String[64];
+        which = 0;
+        for (int i = 0; i < 128; i++){
+            if (yesNoProtein) {
+                proteins[which] = tableInit[i];
+                which++;
+            }
+            yesNoProtein = !yesNoProtein;
         }
         String allData = data.next();
         int codons = allData.length() / 3;
-        System.out.println(allData);
+        String[] dataArray = new String[codons];
+        which = 0;
+        for (int i = 0; i < codons; i++) {
+            dataArray[i] = "";
+        }
+        for (int i = 0; i < codons; i++) {
+            for (int q = 0; q < 3; q++) {
+                dataArray[i] += Character.toString(allData.charAt(which));
+                which++;
+            }
+        }
+        char[] tranlated = new char[codons];
         System.out.println(allData.length());
-        System.out.println(codons);
+        System.out.println(Arrays.toString(dataArray));
+        System.out.println(Arrays.toString(tableInit));
+        System.out.println(Arrays.toString(RNA));
+        System.out.println(Arrays.toString(proteins));
     }
 }
