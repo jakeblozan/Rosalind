3e017992ac4bd9bfea449f37556394d3cd65c861
Jake Blozan <jakeblozan@users.noreply.github.com>
2018-10-25 05:06:03 -0700

    Consensus and Profile work

--- ConsensusandProfile.java
+++ ConsensusandProfile.java
@@ -1,31 +1,40 @@
-import java.util.Scanner;
 import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 /**
  * Write a description of class ConsensusandProfile here.
  * 
  * @author (Jake Blozan) 
- * @version (1/24/17)
+ * @version (10/25/18)
  */
 public class ConsensusandProfile
 {
-	public static void main(String[] args) throws IOException {
-		Scanner data = new Scanner(File("practice.txt"));
-		System.out.println("Fetching data...");
-		int length = data.length();
-		int strings = 0;
-		for (int i = 0; i < length; i++) {
-			if (data.charAt(i) == '>') {
-				strings++;
-			}
-		}
-		int stringLength = 0;
-		for (int i = 1; i < length; i++) {
-			if (data.charAt(i) == '>') {
-				break;
-			}
-			else {
-				stringLength = i;
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        Scanner data = new Scanner(new File("practice.txt"));
+        System.out.println("Fetching data...");
+        String input = data.useDelimiter("\\Z").next();
+        int length = input.length();
+        int strings = 0;
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) == '>') {
+                strings++;
+            }
+        }
+        int stringLength = 0;
+        for (int i = 1; i < length; i++) {
+            if (input.charAt(i) == '>') {
+                break;
+            }
+            else {
+                stringLength = i - 14;
+            }
+        }
+        char[][] table = new char[stringLength][strings];
+        for  (int k = 0; k < strings; k++) {
+            for (int j = 0; j < stringLength; j++) {
+                table[j][k] = input.charAt(j + 14);
+            }
+        }
+        System.out.println(Arrays.deepToString(table));
+    }
 }
